9e98e97bfc47dbf30063ce051419e24266ed7b32
Gearhead-Ayush <ayushbansal1011@gmail.com>
2025-03-26 22:19:11 +0530

    final commit

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:4001",
+    origin: "https://minglify.onrender.com",
     methods: ["GET", "POST"],
   },
 });
